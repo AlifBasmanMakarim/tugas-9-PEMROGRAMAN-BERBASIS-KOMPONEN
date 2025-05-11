bfb64ce87d9f75cd70fbfcbdd3487a376efdca53
Alif_Basman_Makarim <alifbasmanmakarim@student.uir.ac.id>
2025-05-11 15:29:36 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Card from "./components/Card.vue";
+
+const Melihat = () => {
+  alert("Berpindah ke halaman lain");
+};
+</script>
+
+<template>
+  <Card title="Project Baru" @lihat="Melihat" />
+</template>
---
+++ Card.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: "judul default",
+  },
+});
+
+defineEmits(["lihat"]);
+</script>
+
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <p>Menggunakan props, emits dan slot</p>
+    <slot>Klik Tombol Disamping</slot>
+    <button @click="$emit('lihat')">Lihat</button>
+  </div>
+</template>
+
+<style></style>
